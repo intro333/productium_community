ed6d4feb446fa3335c8c0f34c3148c89428ffaff
Dmitry <intro333@ya.ru>
2021-01-13 05:49:39 +0300

    user agreement.

--- src/views/user-agreement/UserAgreement.vue
+++ src/views/user-agreement/UserAgreement.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="user-agreement">
-    <div class="user-agreement-header">
-      <span class="user-agreement-text">Productium</span>
-      <div class="content-hide-mobile">
-        <span class="user-agreement-text user-agreement-text-bold"> юридическая информация</span>
-      </div>
-      <div class="content-hide-desktop p-box-flex-column">
-        <span class="user-agreement-text user-agreement-text-bold">юридическая</span>
-        <span class="user-agreement-text user-agreement-text-bold">информация</span>
+    <div :class="['content-base-box', 'content-base-box-blue']">
+      <div :class="['content-base', 'user-agreement-header']">
+        <span class="user-agreement-text">Productium</span>
+        <div class="content-hide-mobile">
+          <span class="user-agreement-text user-agreement-text-bold"> юридическая информация</span>
+        </div>
+        <div class="content-hide-desktop p-box-flex-column">
+          <span class="user-agreement-text user-agreement-text-bold">юридическая</span>
+          <span class="user-agreement-text user-agreement-text-bold">информация</span>
+        </div>
+        <div class="content-hide-desktop p-button p-button-rounded p-button-rounded1 p-button-select pua-menu-mobile"
+             :class="{'p-button-select-open': isOpenMenuMobile}">
+          <div @click="selectButtonPopup()"
+               class="p-button-text-box">
+            <span class="p-button-text">{{menuTitleByName()}}</span>
+            <img src="@/assets/img/mobile/chevronDown.svg"
+                 class="p-button-select-img"
+                 alt="">
+          </div>
+          <div v-if="isOpenMenuMobile"
+               @click="goToMenuRoute((selectedMenu === 'privacy-policy') ? 'terms-of-use' : 'privacy-policy')"
+               class="p-button-text-box">
+            <span class="p-button-text not-active"
+                  :style="{'padding-top': '16px', 'margin-left': (selectedMenu === 'privacy-policy') ? '-19px' : '-13px'}"
+            >{{invertMenuTitleByName()}}</span>
+          </div>
+        </div>
       </div>
     </div>
     <div :class="['content-base-box']">
@@ -38,17 +57,32 @@
 export default {
   name: "UserAgreement",
   data: () => ({
-    selectedMenu: 'privacy-policy'
+    selectedMenu: 'privacy-policy',
+    isOpenMenuMobile: false
   }),
   created() {
     this.selectedMenu = this.$route.name;
   },
   methods: {
     goToMenuRoute(_route) {
-      this.$router.push({name: _route});
+      if (_route !== this.$route.name) {
+        this.$router.push({name: _route});
+        this.isOpenMenuMobile = false;
+      }
     },
     isActive(_route) {
       return this.$route.name ===_route;
+    },
+    menuTitleByName() {
+      return (this.selectedMenu === 'privacy-policy') ? 'Политика конфиденциальности' :
+          'Пользовательское соглашение';
+    },
+    invertMenuTitleByName() {
+      return (this.selectedMenu === 'terms-of-use') ? 'Политика конфиденциальности' :
+          'Пользовательское соглашение';
+    },
+    selectButtonPopup() {
+      this.isOpenMenuMobile = !this.isOpenMenuMobile;
     },
   },
   watch: {
