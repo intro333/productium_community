ebb80c585214f4bc1ce3c47e565c919771fe6cdb
Дмитрий Держаев <intro8360@gmail.com>
2021-03-28 22:52:54 +0300

    init data.

--- src/store/index.js
+++ src/store/index.js
@@ -132,7 +132,7 @@ export default new Vuex.Store({
     },
     fetchIpAddressAndSetOsInfo({ commit }) {
       return new Promise((resolve, reject) => {
-        window.axios.get('https://cors-anywhere.herokuapp.com/http://api.ipify.org/?format=json', {})
+        window.axios.get('https://api.ipify.org?format=json', {})
           // axios.get('http://api.ipify.org/?format=json', {})
           .then(response => {
             const ip = response.data.ip;
